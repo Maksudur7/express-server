6e6ba9ade7a8ec0478fb8c05046b710fae1d3fac
Maksudr <maksudurrahamanmishu7@gmail.com>
2025-12-04 08:54:40 +0600

    add app.js file

--- src/modules/user/user.routs.ts
+++ src/modules/user/user.routs.ts
@@ -3,8 +3,8 @@ import { userControllers } from './user.controller';
 import auth from '../../middleware/auth';
 const Router = express.Router();
 Router.post("/", userControllers.createUser)  //done
-Router.get("/", auth(), userControllers.getUser)
-Router.get("/:id",userControllers.getSingleUser)
+Router.get("/", auth("Admin"), userControllers.getUser)
+Router.get("/:id", auth("admin", "user"), userControllers.getSingleUser)
 Router.put("/:id", userControllers.updateUser)
 Router.delete("/:id", userControllers.deletUser)
 export const userRoutes = Router;
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,6 @@
-import express, { NextFunction, Request, Response } from "express";
+import app from "./app"
 import config from "./config";
-import initDB from "./config/db";
-import logger from "./middleware/logger";
-import { userRoutes } from "./modules/user/user.routs";
-import { todoRouter } from "./modules/todo/todo.routs";
-import { authRoutes } from "./modules/auth/auth.route";
-
-const app = express()
-//parser
 const port = config.port;
-app.use(express.json())
-// app.use(express.urlencoded())
-// database holo neon 
-initDB();
-app.get('/', logger, (req: Request, res: Response) => {
-    res.send('Hello Maksudur')
-})
-//post users crad
-app.use("/users", userRoutes )
-app.use("/todos", todoRouter)
-app.use("/auth", authRoutes)
-
-// 404 handel kora
-app.use((req: Request, res: Response) => {
-    res.status(404).json({
-        success: false,
-        message: "Route not found",
-        path: req.path
-    })
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
